acf77b142c11ea0d75410a0d1d83d51ca44368f6
AmaySinghh <amaysingh2626@gmail.com>
2025-11-18 13:23:53 +0530

    Implement user input and display in BasicIO
    Added user input functionality to read name and age.

--- 01_Java_Basics/BasicIO.java
+++ 01_Java_Basics/BasicIO.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
+public class BasicIO {
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in); // Create Scanner object
+
+        // Taking input
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+
+        // Displaying output
+        System.out.println("--- User Info ---");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+  }
+}
 
